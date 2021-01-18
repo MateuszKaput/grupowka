d2243bd239fa4f82143e24367d2319393a2d0a12
CzyzDaniel <deutrycik@gmail.com>
2021-01-18 13:14:30 +0100

    help exit dodany

--- Grupowka/src/grupowka/Grupowka.java
+++ Grupowka/src/grupowka/Grupowka.java
@@ -5,6 +5,12 @@
  */
 package grupowka;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author xXx
@@ -14,8 +20,54 @@ public class Grupowka {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+    
+      
+      System.out.println("Podaj scierzke do pliku: ");
+      Scanner scan = new Scanner(System.in);
+      String scierzka = scan.next();
+      Boolean czyexit = false;
+
+      while(!czyexit)
+      {
+                while(scierzka.equals("") || scierzka == null)
+                {
+                  System.out.println("Nie podano wartosci, po pomoc wpisz : help ");
+                }
+
+                if(scierzka.equals("help"))
+                {
+                  System.out.println("Mozliwe komendy: ");
+                  System.out.println("exit - wyjście z programu ");
+                }
+                
+                if(scierzka.equals("exit"))
+                {
+                    czyexit = true;
+                    break;
+                }
+
+                try
+                {
+                        int count =0;
+                        File file = new File(scierzka);
+                        FileInputStream fis = new FileInputStream(file);
+                        byte[] bytesArray = new byte[(int)file.length()];
+                        fis.read(bytesArray);
+                        String s = new String(bytesArray);
+                        String [] data = s.split(" ");
+                        for (int i=0; i<data.length; i++) {
+                           count++;
+                        }
+                        System.out.println("Ilosc slow w pliku " +count);
+                }
+                catch(FileNotFoundException e)
+                {
+                   System.out.println("Niepoprawna scierzka ");
+                }
+                
+             scierzka = scan.next();
+      }
+   }
     
 }
